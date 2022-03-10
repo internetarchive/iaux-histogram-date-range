3d2654626676eaa6c2c4648d44c41112c8e2850d
Jason Buckner <jason@jasonbuckner.com>
2022-03-10 13:01:32 -0800

    Update demo with additional usage

--- demo/app-root.ts
+++ demo/app-root.ts
@@ -1,6 +1,14 @@
 import { LitElement, html, TemplateResult } from 'lit';
-import { customElement } from 'lit/decorators.js';
+import { customElement, state } from 'lit/decorators.js';
 import '../src/histogram-date-range';
+
+interface DataSource {
+  minDate: unknown;
+  maxDate: unknown;
+  minSelectedDate: unknown;
+  maxSelectedDate: unknown;
+  bins: number[];
+}
 
 /**
  * This is mainly to test the histogram-date-range within
@@ -8,14 +16,44 @@ import '../src/histogram-date-range';
  */
 @customElement('app-root')
 export class AppRoot extends LitElement {
+  @state() dataSource: DataSource = {
+    minDate: 1955,
+    maxDate: 2000,
+    minSelectedDate: 1955,
+    maxSelectedDate: 2000,
+    bins: [
+      1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+    ],
+  };
+
   render(): TemplateResult {
     return html`
       <histogram-date-range
-        .minDate=${1400}
-        .maxDate=${2021}
+        .minDate=${this.dataSource?.minDate}
+        .maxDate=${this.dataSource?.maxDate}
+        .minSelectedDate=${this.dataSource?.minSelectedDate}
+        .maxSelectedDate=${this.dataSource?.maxSelectedDate}
         .updateDelay=${1000}
-        .bins=${[74, 67, 17, 66, 49, 93]}
+        .bins=${this.dataSource?.bins}
       ></histogram-date-range>
+
+      <button @click=${this.randomize}>Randomize</button>
     `;
+  }
+
+  private randomize() {
+    const minDate = Math.round(Math.random() * 1000);
+    const maxDate = minDate + Math.round(Math.random() * 1000);
+    // generate random bins
+    const bins = Array.from({ length: 20 }, () =>
+      Math.floor(Math.random() * minDate)
+    );
+    this.dataSource = {
+      minDate,
+      maxDate,
+      minSelectedDate: minDate,
+      maxSelectedDate: maxDate,
+      bins: bins,
+    };
   }
 }
