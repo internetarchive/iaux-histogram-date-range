cad2bf439599ed177fd1db67d0426d6cb17697ed
Jason Buckner <jasonb@archive.org>
2025-01-17 14:21:29 -0800

    Update ia-activity-indicator and dayjs imports (#27)
    * Update ia-activity-indicator and dayjs imports
    
    * Bump version
    
    * Update CI
    
    * Update docs
    
    * Organize imports

--- docs/_snowpack/pkg/lit/decorators.js
+++ docs/_snowpack/pkg/lit/decorators.js
@@ -1,27 +1,10 @@
-/**
- * @license
- * Copyright 2017 Google LLC
- * SPDX-License-Identifier: BSD-3-Clause
- */
-const n=n=>e=>"function"==typeof e?((n,e)=>(window.customElements.define(n,e),e))(n,e):((n,e)=>{const{kind:t,elements:i}=e;return {kind:t,elements:i,finisher(e){window.customElements.define(n,e);}}})(n,e);
+import { n } from '../common/query-assigned-elements-61aefe32.js';
+export { e as customElement, n as property } from '../common/query-assigned-elements-61aefe32.js';
 
 /**
  * @license
  * Copyright 2017 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
- */
-const i=(i,e)=>"method"===e.kind&&e.descriptor&&!("value"in e.descriptor)?{...e,finisher(n){n.createProperty(e.key,i);}}:{kind:"field",key:Symbol(),placement:"own",descriptor:{},originalKey:e.key,initializer(){"function"==typeof e.initializer&&(this[e.key]=e.initializer.call(this));},finisher(n){n.createProperty(e.key,i);}};function e(e){return (n,t)=>void 0!==t?((i,e,n)=>{e.constructor.createProperty(n,i);})(e,n,t):i(e,n)}
+ */function t(t){return n({...t,state:!0})}
 
-/**
- * @license
- * Copyright 2017 Google LLC
- * SPDX-License-Identifier: BSD-3-Clause
- */function t(t){return e({...t,state:!0})}
-
-/**
- * @license
- * Copyright 2021 Google LLC
- * SPDX-License-Identifier: BSD-3-Clause
- */var n$1;const e$1=null!=(null===(n$1=window.HTMLSlotElement)||void 0===n$1?void 0:n$1.prototype.assignedElements)?(o,n)=>o.assignedElements(n):(o,n)=>o.assignedNodes(n).filter((o=>o.nodeType===Node.ELEMENT_NODE));
-
-export { n as customElement, e as property, t as state };
+export { t as state };
